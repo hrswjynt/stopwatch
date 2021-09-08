01267696834d6ba6b7ddd5f6ffcc80f15fd72dfa
Haris Wijayanto <hrswjynto@gmail.com>
2021-09-08 11:24:42 +0700

    Buat testing

--- scripts/main.js
+++ scripts/main.js
@@ -37,7 +37,6 @@ function clock() {
 		"clock"
 	).innerHTML = `${displayHours}:${displayMinutes}:${dispaySeconds}`;
 }
-window.setInterval(clock, 1000);
 
 // Stopwatch
 function stopwatch() {
@@ -86,60 +85,13 @@ function reset() {
 		.forEach((element) => (element.innerHTML = "00:00:00"));
 }
 
-// Finish
-function user1() {
-	twoDigits(seconds, minutes, hours);
+// Finish button
+const finishButtons = document.querySelectorAll("finish");
+finishButtons.forEach((element) => {
+	element.addEventListener("click", function (el) {
+		const hourText = el.previousSibling.innerText; // previousSibling maksudnye element sebelum finishButton yaitu laptime
+		console.log(hourText);
+	});
+});
 
-	document.getElementById(
-		"user-01"
-	).innerHTML = `${displayHours}:${displayMinutes}:${dispaySeconds}`;
-}
-function user2() {
-	twoDigits(seconds, minutes, hours);
-
-	document.getElementById(
-		"user-02"
-	).innerHTML = `${displayHours}:${displayMinutes}:${dispaySeconds}`;
-}
-function user3() {
-	twoDigits(seconds, minutes, hours);
-
-	document.getElementById(
-		"user-03"
-	).innerHTML = `${displayHours}:${displayMinutes}:${dispaySeconds}`;
-}
-function user4() {
-	twoDigits(seconds, minutes, hours);
-
-	document.getElementById(
-		"user-04"
-	).innerHTML = `${displayHours}:${displayMinutes}:${dispaySeconds}`;
-}
-function user5() {
-	twoDigits(seconds, minutes, hours);
-
-	document.getElementById(
-		"user-05"
-	).innerHTML = `${displayHours}:${displayMinutes}:${dispaySeconds}`;
-}
-function user6() {
-	twoDigits(seconds, minutes, hours);
-
-	document.getElementById(
-		"user-06"
-	).innerHTML = `${displayHours}:${displayMinutes}:${dispaySeconds}`;
-}
-function user7() {
-	twoDigits(seconds, minutes, hours);
-
-	document.getElementById(
-		"user-07"
-	).innerHTML = `${displayHours}:${displayMinutes}:${dispaySeconds}`;
-}
-function user8() {
-	twoDigits(seconds, minutes, hours);
-
-	document.getElementById(
-		"user-08"
-	).innerHTML = `${displayHours}:${displayMinutes}:${dispaySeconds}`;
-}
+window.setInterval(clock, 1000);
